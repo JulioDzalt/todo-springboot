1ad574f11bacaa8c83ceb6db01c19f8d2dfb22be
Julio Cesar Diaz Altamirano <Julio.Cesar.Diaz.Altamirano@ibm.com>
2022-04-01 17:24:45 -0600

    Added springboot base project

---
+++ src/main/java/com/example/todo_crud/TodoCrudApplication.java
@@ -0,0 +1,14 @@
+package com.example.todo_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoCrudApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(TodoCrudApplication.class, args);
+	}
+
+}
