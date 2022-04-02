3c91019b0fa9e7b6dd3ab45ad72e9541f2f80a7b
Julio Cesar Diaz Altamirano <Julio.Cesar.Diaz.Altamirano@ibm.com>
2022-04-01 18:17:13 -0600

    Added mock method

--- src/main/java/com/example/todo_crud/controllers/TodoController.java
+++ src/main/java/com/example/todo_crud/controllers/TodoController.java
@@ -1,5 +1,12 @@
 package com.example.todo_crud.controllers;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.example.todo_crud.models.TodoCategoriesE;
+import com.example.todo_crud.models.TodoModel;
+import com.example.todo_crud.models.TodoStatusE;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +16,25 @@ public class TodoController {
     @GetMapping("/version")
     public String version() {
         return "1.1";
+    }
+
+    @GetMapping("/todo")
+    public TodoModel getTodobyId() {
+        TodoModel t = new TodoModel();
+        t.setId(1);
+        t.setTitle("La de albert");
+        t.setContent("hola mundo");
+        t.setDateCreation(new Date());
+        t.setStatus(TodoStatusE.IN_PROGRESS);
+        t.setCategorias(
+            new ArrayList<TodoCategoriesE>() {
+                {
+                    add(TodoCategoriesE.Home); 
+                    add(TodoCategoriesE.PersonalBelongings);
+                }
+            }
+        );
+        
+        return  t;
     }
 }
