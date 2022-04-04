2405124303193ba493fa3f48b9527026334a8c19
Julio Cesar Diaz Altamirano <Julio.Cesar.Diaz.Altamirano@ibm.com>
2022-04-04 10:58:07 -0500

    Added validations on model

--- src/main/java/com/example/todo_crud/controllers/TodoController.java
+++ src/main/java/com/example/todo_crud/controllers/TodoController.java
@@ -1,15 +1,21 @@
 package com.example.todo_crud.controllers;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.TimeZone;
+
+import javax.validation.Valid;
 
 import com.example.todo_crud.models.TodoCategoriesE;
 import com.example.todo_crud.models.TodoModel;
 import com.example.todo_crud.models.TodoStatusE;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -51,5 +57,13 @@ public class TodoController {
         );
         
         return  t;
+    }
+
+    @PostMapping("/todo")
+    public TodoModel createTodo(@Valid @RequestBody TodoModel todo) {
+        System.out.println(todo);
+        System.out.println(todo.getDateCreation());
+
+        return todo;
     }
 }
--- src/main/java/com/example/todo_crud/models/TodoModel.java
+++ src/main/java/com/example/todo_crud/models/TodoModel.java
@@ -3,6 +3,13 @@ package com.example.todo_crud.models;
 import java.util.ArrayList;
 import java.util.Date;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,10 +20,19 @@ import lombok.NoArgsConstructor;
 public class TodoModel {
     
     private int id;
+
+    @NotBlank(message = "Title is mandatory")
     private String title;
+    
+    @NotBlank(message = "Content is mandatory")
     private String content;
+    
+    @NotNull(message = "The dateCreation is required.")
+    @DateISOValidation(message = "The dateCreation is invalid.")
     private String dateCreation;
+    
     private TodoStatusE status;
+
     private ArrayList<TodoCategoriesE> categorias;
 
 }
